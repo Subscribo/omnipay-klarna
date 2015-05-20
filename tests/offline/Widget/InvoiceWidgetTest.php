547d2be1fd6a63870f339c2d7ccfad224ea2ddf8
slavo <slavo2@users.noreply.github.com>
2015-05-20 14:32:30 +0200

    Adding and updating tests for InvoiceWidget

--- tests/offline/Widget/InvoiceWidgetTest.php
+++ tests/offline/Widget/InvoiceWidgetTest.php
@@ -145,6 +145,7 @@ class InvoiceWidgetTest extends PHPUnit_Framework_TestCase
         $this->assertSame($expectedLogoUrl3, $widget->renderLogoUrl(['color' => 'nonexistent']));
     }
 
+
     public function testRenderTooltip()
     {
         $merchantId = uniqid();
@@ -173,6 +174,7 @@ class InvoiceWidgetTest extends PHPUnit_Framework_TestCase
         $this->assertSame($expectedTooltip2, $widget->renderTooltip($parameters, false));
     }
 
+
     public function testRenderTermsInvoiceHtml()
     {
         $merchantId = uniqid();
@@ -181,6 +183,9 @@ class InvoiceWidgetTest extends PHPUnit_Framework_TestCase
             'country' => 'AT',
             'language' => 'de',
         ]);
+        $this->assertSame($merchantId, $widget->getMerchantId());
+        $this->assertSame('AT', $widget->getCountry());
+        $this->assertSame('de', $widget->getLanguage());
 
         $basicPattern = '<span id="klarna-invoice-rechnungsbedingungen-n([0-9]+)"></span>'."\n"
                       . "<script>new Klarna.Terms.Invoice\\({ el: 'klarna-invoice-rechnungsbedingungen-n\\1', eid: '".$merchantId."', locale: 'de_at', charge: '0' }\\);</script>";
@@ -188,10 +193,12 @@ class InvoiceWidgetTest extends PHPUnit_Framework_TestCase
                 . '<script src="https://cdn.klarna.com/public/kitt/toc/v1.1/js/klarna.terms.min.js"></script>'."\n";
         $this->assertRegExp('#^'.$loadJs.$basicPattern.'$#', $widget->renderTermsInvoiceHtml());
         $this->assertRegExp('#^'.$basicPattern.'$#', $widget->renderTermsInvoiceHtml([], false));
+
         $expectedHtml = '<span id="some-element-id"></span>'."\n"
             . "<script>new Klarna.Terms.Invoice({ el: 'some-element-id', eid: '".$merchantId."', locale: 'de_at', charge: '0' });</script>";
         $this->assertSame($loadJs.$expectedHtml, $widget->renderTermsInvoiceHtml([], true, 'some-element-id'));
         $this->assertSame($expectedHtml, $widget->renderTermsInvoiceHtml([], false, 'some-element-id'));
+
         $parameters = [
             'outputDeviceType' => 'mobile',
             'charge' => '1.05',
@@ -201,5 +208,86 @@ class InvoiceWidgetTest extends PHPUnit_Framework_TestCase
             . "<script>new Klarna.Terms.Invoice\\({ el: 'klarna-invoice-rechnungsbedingungen-n\\1', eid: '".$merchantId."', locale: 'de_at', charge: '1.05', type: 'mobile', linkClassName: 'someClass' }\\);</script>";
         $this->assertRegExp('#^'.$loadJs.$extendedPattern.'$#', $widget->renderTermsInvoiceHtml($parameters));
         $this->assertRegExp('#^'.$extendedPattern.'$#', $widget->renderTermsInvoiceHtml($parameters, false));
+    }
+
+
+    public function testRenderTermsConsentHtml()
+    {
+        $merchantId = uniqid();
+        $widget = new InvoiceWidget([
+            'merchantId' => $merchantId,
+            'country' => 'AT',
+            'language' => 'de',
+        ]);
+        $this->assertSame($merchantId, $widget->getMerchantId());
+        $this->assertSame('AT', $widget->getCountry());
+        $this->assertSame('de', $widget->getLanguage());
+
+        $basicPattern = 'Mit der Übermittlung der für die Abwicklung der gewählten Klarna Zahlungsmethode und einer Identitäts- und Bonitätsprüfung erforderlichen Daten an Klarna bin ich einverstanden. Meine <span id="klarna-consent-einwilligung-n([0-9]+)"></span> kann ich jederzeit mit Wirkung für die Zukunft widerrufen. Es gelten die AGB des Händlers.'."\n"
+            . "<script>new Klarna.Terms.Consent\\({ el: 'klarna-consent-einwilligung-n\\1', eid: '".$merchantId."', locale: 'de_at' }\\);</script>";
+        $loadJs = '<script src="https://cdn.klarna.com/public/kitt/core/v1.0/js/klarna.min.js"></script>'."\n"
+            . '<script src="https://cdn.klarna.com/public/kitt/toc/v1.1/js/klarna.terms.min.js"></script>'."\n";
+        $this->assertRegExp('#^'.$loadJs.$basicPattern.'$#', $widget->renderTermsConsentHtml());
+        $this->assertRegExp('#^'.$basicPattern.'$#', $widget->renderTermsConsentHtml([], false));
+
+        $expectedHtml = 'Mit der Übermittlung der für die Abwicklung der gewählten Klarna Zahlungsmethode und einer Identitäts- und Bonitätsprüfung erforderlichen Daten an Klarna bin ich einverstanden. Meine <span id="some-element-id"></span> kann ich jederzeit mit Wirkung für die Zukunft widerrufen. Es gelten die AGB des Händlers.'."\n"
+            . "<script>new Klarna.Terms.Consent({ el: 'some-element-id', eid: '".$merchantId."', locale: 'de_at' });</script>";
+        $this->assertSame($loadJs.$expectedHtml, $widget->renderTermsConsentHtml([], true, 'some-element-id'));
+        $this->assertSame($expectedHtml, $widget->renderTermsConsentHtml([], false, 'some-element-id'));
+
+        $parameters = [
+            'outputDeviceType' => 'desktop',
+            'AGBUrl' => 'https://your.web.site.example/path/to/AGB',
+            'linkClassName' => 'someClass',
+        ];
+        $extendedPattern = 'Mit der Übermittlung der für die Abwicklung der gewählten Klarna Zahlungsmethode und einer Identitäts- und Bonitätsprüfung erforderlichen Daten an Klarna bin ich einverstanden. Meine <span id="klarna-consent-einwilligung-n([0-9]+)"></span> kann ich jederzeit mit Wirkung für die Zukunft widerrufen. Es gelten <a href="https://your.web.site.example/path/to/AGB" target="_blank">die AGB</a> des Händlers.'."\n"
+            . "<script>new Klarna.Terms.Consent\\({ el: 'klarna-consent-einwilligung-n\\1', eid: '".$merchantId."', locale: 'de_at', type: 'desktop', linkClassName: 'someClass' }\\);</script>";
+        $this->assertRegExp('#^'.$loadJs.$extendedPattern.'$#', $widget->renderTermsConsentHtml($parameters));
+        $this->assertRegExp('#^'.$extendedPattern.'$#', $widget->renderTermsConsentHtml($parameters, false));
+    }
+
+
+    public function testRenderTermsAccountHtml()
+    {
+        $merchantId = uniqid();
+        $widget = new InvoiceWidget([
+            'merchantId' => $merchantId,
+            'country' => 'DE',
+            'language' => 'de',
+        ]);
+        $this->assertSame($merchantId, $widget->getMerchantId());
+        $this->assertSame('DE', $widget->getCountry());
+        $this->assertSame('de', $widget->getLanguage());
+
+        $basicPattern = '<span id="klarna-account-lesen-sie-mehr-n([0-9]+)"></span>'."\n"
+            . "<script>new Klarna.Terms.Account\\({ el: 'klarna-account-lesen-sie-mehr-n\\1', eid: '".$merchantId."', locale: 'de_de' }\\);</script>";
+        $loadJs = '<script src="https://cdn.klarna.com/public/kitt/core/v1.0/js/klarna.min.js"></script>'."\n"
+            . '<script src="https://cdn.klarna.com/public/kitt/toc/v1.1/js/klarna.terms.min.js"></script>'."\n";
+        $this->assertRegExp('#^'.$loadJs.$basicPattern.'$#', $widget->renderTermsAccountHtml());
+        $this->assertRegExp('#^'.$basicPattern.'$#', $widget->renderTermsAccountHtml([], false));
+
+        $expectedHtml = '<span id="some-element-id"></span>'."\n"
+            . "<script>new Klarna.Terms.Account({ el: 'some-element-id', eid: '".$merchantId."', locale: 'de_de' });</script>";
+        $this->assertSame($loadJs.$expectedHtml, $widget->renderTermsAccountHtml([], true, 'some-element-id'));
+        $this->assertSame($expectedHtml, $widget->renderTermsAccountHtml([], false, 'some-element-id'));
+
+        $parameters = [
+            'outputDeviceType' => 'mobile',
+            'linkClassName' => 'someClass',
+        ];
+        $extendedPattern = '<span id="klarna-account-lesen-sie-mehr-n([0-9]+)"></span>'."\n"
+            . "<script>new Klarna.Terms.Account\\({ el: 'klarna-account-lesen-sie-mehr-n\\1', eid: '".$merchantId."', locale: 'de_de', type: 'mobile', linkClassName: 'someClass' }\\);</script>";
+        $this->assertRegExp('#^'.$loadJs.$extendedPattern.'$#', $widget->renderTermsAccountHtml($parameters));
+        $this->assertRegExp('#^'.$extendedPattern.'$#', $widget->renderTermsAccountHtml($parameters, false));
+    }
+
+
+    public function testJavascriptLoaders()
+    {
+        $expectedJavascriptLoader = '<script async src="https://cdn.klarna.com/1.0/code/client/all.js"></script>';
+        $this->assertSame($expectedJavascriptLoader, InvoiceWidget::assembleLoadJavascript());
+        $expectedTermsJavascriptLoader = '<script src="https://cdn.klarna.com/public/kitt/core/v1.0/js/klarna.min.js"></script>'."\n"
+                                       . '<script src="https://cdn.klarna.com/public/kitt/toc/v1.1/js/klarna.terms.min.js"></script>';
+        $this->assertSame($expectedTermsJavascriptLoader, InvoiceWidget::assembleLoadTermsJavascript());
     }
 }
